525970ee691198e1d803700ecc9a35c13c645482
BlackBracken <me@bracken.black>
2018-10-10 11:14:16 +0900

    hide needless properties

--- src/main/kotlin/black/bracken/chicken/key/DownModelKey.kt
+++ src/main/kotlin/black/bracken/chicken/key/DownModelKey.kt
@@ -8,8 +8,8 @@ import com.beust.klaxon.JsonObject
  * @author BlackBracken
  */
 class DownModelKey<M : JsonModel, R : JsonModel>(
-        val id: String,
-        val instantiate: (JsonObject) -> R
+        private val id: String,
+        private val instantiate: (JsonObject) -> R
 ) : ModelKey<M, ExtractableJsonModel<R>>(
         { model -> ExtractableJsonModel(instantiate(model.jsonObject[id] as JsonObject)) }
 )
